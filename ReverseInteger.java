917cec691c7491ca583b7e0b7aeafb8750bbe35f
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2024-12-26 22:51:20 +0530

    LEETCODE -> 7. Reverse Integer

---
+++ ReverseInteger.java
@@ -0,0 +1,22 @@
+class ReverseInteger {
+    public int reverse(int x) {
+        boolean neg=false;
+        if(x<0){
+            neg=true;
+            x= -x;
+        }
+        long rev=0;
+        while(x>0){
+            int last=x%10;
+            x=x/10;
+            rev=rev*10+last;
+        }
+        if(neg){
+            rev=-rev;
+        }
+        if(rev > Integer.MAX_VALUE||rev < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)rev;
+    }
+}
