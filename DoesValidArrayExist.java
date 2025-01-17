c333d19f9e50d5101bcce12b782ab6c0369537c0
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-01-17 17:53:55 +0530

    LEETCODE -> 2683. Neighboring Bitwise XOR

---
+++ DoesValidArrayExist.java
@@ -0,0 +1,11 @@
+class DoesValidArrayExist {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n=derived.length;
+        int sum=0;
+
+        for(int i=0; i<n; i++){
+            sum^=derived[i];
+        }
+        return sum==0;
+    }
+}
