088cf9ee6b9ea6d7284fda8586e5e8c8c320d551
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-01-24 16:01:25 +0530

    LEETCODE -> 802. Find Eventual Safe States

---
+++ EventualSafeNodes.java
@@ -0,0 +1,96 @@
+import java.util.AbstractList;
+
+class EventualSafeNodes {
+    List<Integer> safeNodes;
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        // int n = graph.length;
+        // List<List<Integer>> reverseAdj = new ArrayList<>();
+        // List<Integer> safeNodes = new ArrayList<>();
+        // int[] inDegree = new int[n];
+
+        // for(int i = 0; i < n; i++){
+        //     reverseAdj.add(new ArrayList<>());
+        // }
+
+        // for(int i = 0; i < n; i++){
+        //     for(int neighborNode: graph[i]){
+        //         reverseAdj.get(neighborNode).add(i);
+        //         inDegree[i]++;
+        //     }
+        // }
+
+        // Queue<Integer> queue = new LinkedList<Integer>();
+        // for(int i = 0; i < n; i++){
+        //     if(inDegree[i] == 0){
+        //         queue.offer(i);
+        //     }
+        // }
+
+        // while(!queue.isEmpty()){
+        //     int safeNode = queue.poll();
+        //     safeNodes.add(safeNode);
+
+        //     for(int neighbor : reverseAdj.get(safeNode)){
+        //         inDegree[neighbor]--;
+        //         if(inDegree[neighbor] == 0){
+        //             queue.offer(neighbor);
+        //         }
+        //     }
+        // }
+        // Collections.sort(safeNodes);
+        // return safeNodes;
+        return new AbstractList<Integer>(){
+            @Override
+            public Integer get(int index){
+                init();
+                return safeNodes.get(index);
+            }
+
+            @Override
+            public int size(){
+                init();
+                return safeNodes.size();
+            }
+
+            private void init(){
+                if(safeNodes != null) return;
+
+                int n = graph.length;
+                List<List<Integer>> reverseAdj = new ArrayList<>();
+                safeNodes = new ArrayList<>();
+                int[] inDegree = new int[n];
+
+                for(int i = 0; i < n; i++){
+                    reverseAdj.add(new ArrayList<>());
+                }
+
+                for(int i = 0; i < n; i++){
+                    for(int neighborNode: graph[i]){
+                        reverseAdj.get(neighborNode).add(i);
+                        inDegree[i]++;
+                    }
+                }
+
+                Queue<Integer> queue = new LinkedList<Integer>();
+                for(int i = 0; i < n; i++){
+                    if(inDegree[i] == 0){
+                        queue.offer(i);
+                    }
+                }
+
+                while(!queue.isEmpty()){
+                    int safeNode = queue.poll();
+                    safeNodes.add(safeNode);
+
+                    for(int neighbor : reverseAdj.get(safeNode)){
+                        inDegree[neighbor]--;
+                        if(inDegree[neighbor] == 0){
+                            queue.offer(neighbor);
+                        }
+                    }
+                }
+                Collections.sort(safeNodes);
+            }
+        };   
+    }
+}
