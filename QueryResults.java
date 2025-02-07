1db0df2273a096a1c73b49e6f1c7c18545e3da52
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-02-07 19:40:29 +0530

    LEETCODE -> 3160. Find the Number of Distinct Colors Among the Balls

---
+++ QueryResults.java
@@ -0,0 +1,26 @@
+class QueryResults {
+    public int[] queryResults(int limit, int[][] queries) {
+        Map<Integer,Integer> node = new HashMap<>();
+        Map<Integer,Integer> color = new HashMap<>();
+        int ans[]=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int it[]=queries[i];
+            if(node.containsKey(it[0])){
+                if(node.get(it[0])==it[1]) {
+                    ans[i]=color.size();continue;
+                }
+                else if (color.get(node.get(it[0]))<=1) color.remove(node.get(it[0]));
+                else
+                color.put(node.get(it[0]),color.get(node.get(it[0]))-1);
+                node.put(it[0],it[1]);
+                color.put(it[1],color.getOrDefault(it[1],0)+1);
+            }
+            else{
+                node.put(it[0],it[1]);
+                color.put(it[1],color.getOrDefault(it[1],0)+1);
+            }
+            ans[i]=color.size();
+        }
+        return ans;
+    }
+}
