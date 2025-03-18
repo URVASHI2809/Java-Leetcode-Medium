d266f46677825bf5d6e497986a3dbfd3fd2ff50b
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-03-18 17:34:39 +0530

    LEETCODE -> 2401. Longest Nice Subarray

---
+++ LongestNiceSubarray.java
@@ -0,0 +1,21 @@
+class LongestNiceSubarray {
+    public int longestNiceSubarray(int[] nums) {
+        int bit = 0;
+        int max = 1;
+        int s = 0;
+        int e = 0;
+        int n = nums.length;
+         while(e < n){
+          while( (bit& nums[e]) != 0){
+            bit^=nums[s++];
+          }
+
+          bit|=nums[e];
+          if(e-s+1 > max){
+            max = e-s+1;
+          }
+          e++;
+         }
+     return max; 
+    }
+}
