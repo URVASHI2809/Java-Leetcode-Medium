4063bf0cd98cd551ef8d9f98ca092f142cb7f54f
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-03-24 18:52:40 +0530

    LEETCODE -> 3169. Count Days Without Meetings

---
+++ CountDays.java
@@ -0,0 +1,34 @@
+class CountDays {
+    public int countDays(int days, int[][] meetings) {
+        int freeDays = days;
+        TreeMap<Integer, Integer> meetingDays = new TreeMap<>();
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            int overlapDays = 0;
+
+            Map.Entry<Integer, Integer> previousMeetingDays = meetingDays.floorEntry(start);
+            if (previousMeetingDays != null && previousMeetingDays.getValue() >= start - 1) {
+                if (previousMeetingDays.getValue() >= end) continue;
+                overlapDays = previousMeetingDays.getValue() - previousMeetingDays.getKey() + 1;
+                start = previousMeetingDays.getKey();
+            }
+
+            Map.Entry<Integer, Integer> nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            while (nextMeetingDays != null && nextMeetingDays.getKey() <= end + 1) {
+                meetingDays.remove(nextMeetingDays.getKey());
+                overlapDays += nextMeetingDays.getValue() - nextMeetingDays.getKey() + 1;
+                if (nextMeetingDays.getValue() >= end) {
+                    end = nextMeetingDays.getValue();
+                    break;
+                }
+                nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            }
+
+            meetingDays.put(start, end);
+            freeDays -= (end - start + 1) - overlapDays;
+            if (freeDays == 0) break;
+        }
+        return freeDays;
+    }
+}
