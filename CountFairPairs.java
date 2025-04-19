31abdbaf8bb89ca4565296f2c639dce1396d3a5c
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-04-19 22:54:12 +0530

    LEETCODE -> 2563. Count the Number of Fair Pairs

---
+++ CountFairPairs.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class CountFairPairs {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return count(nums, upper) - count(nums, lower - 1);
+    }
+
+    private long count(int[] nums, int target) {
+        long res = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) right--;
+            else res += right - left++;
+        }
+        return res;
+    }
+}
