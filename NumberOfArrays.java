b9e61ad337c1592f7d0db8021b63a771f9d807c3
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-04-21 15:23:39 +0530

    LEETCODE -> 2145. Count the Hidden Sequences

---
+++ NumberOfArrays.java
@@ -0,0 +1,11 @@
+class NumberOfArrays {
+   public int numberOfArrays(int[] differences, int lower, int upper) {
+        long start = 0, max = 0, min = 0;
+        for(int diff : differences){
+            start += diff;
+            max = (max<start)?start:max;
+            min = (min>start)?start:min;
+        }
+        return (int)Math.max(0,(upper-lower)-(max-min)+1);
+    }
+}
