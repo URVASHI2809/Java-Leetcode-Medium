621b53da7ce6600015d0235fb82c47714455a531
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-07-22 18:32:52 +0530

    LEETCODE -> 1695. Maximum Erasure Value

---
+++ MaximumUniqueSubarray.java
@@ -0,0 +1,22 @@
+class MaximumUniqueSubarray {
+    public int maximumUniqueSubarray(int[] nums) {
+        int ans = 0;
+        int[] record = new int[10001];
+        int sum = 0;
+        int n = nums.length;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            int cur = nums[right];
+            sum += cur;
+            record[cur]++;
+            while (record[cur] > 1) {
+                sum -= nums[left];
+                record[nums[left]]--;
+                left++;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
