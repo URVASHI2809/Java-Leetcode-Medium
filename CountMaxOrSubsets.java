f92722a47d6caeea3b7a59d19fcabdaf7c56142e
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-07-29 01:10:19 +0530

    LEETCODE -> 2044. Count Number of Maximum Bitwise-OR Subsets

---
+++ CountMaxOrSubsets.java
@@ -0,0 +1,18 @@
+class CountMaxOrSubsets {
+    int count=0;
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr=0;
+        for(int a:nums) maxOr |= a;
+        backtrack(nums,maxOr,0,0);
+        return count;
+    }
+    void backtrack(int[] nums,int targetOr, int currOr,int index){
+        if(currOr==targetOr){
+            count = count + (1<<(nums.length-index));
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            backtrack(nums,targetOr,currOr|nums[i],i+1);
+        }
+    }
+}
