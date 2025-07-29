6dfe4d8696c151db869982e21ace182e9ba829cb
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-07-29 17:20:16 +0530

    LEETCODE -> 2411. Smallest Subarrays With Maximum Bitwise OR

---
+++ SmallestSubarrays.java
@@ -0,0 +1,15 @@
+class SmallestSubarrays {
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if ((nums[j] | nums[i]) == nums[j]) break;
+                nums[j] |= nums[i];
+                ret[j] = i - j + 1;
+            }
+        }
+        return ret;
+    }
+}
