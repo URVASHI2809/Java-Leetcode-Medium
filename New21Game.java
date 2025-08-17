dbd89e1254406a453906ad3d09cff926d1674d38
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-08-18 00:45:45 +0530

    LEETCODE -> 837. New 21 Game

---
+++ New21Game.java
@@ -0,0 +1,19 @@
+class New21Game {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0) return 1.0;
+        double windowSum = 0.0;
+        for (int i = k; i < k + maxPts; i++) {
+            windowSum += (i <= n) ? 1 : 0;
+        }
+        var dp = new HashMap<Integer, Double>();
+        for (int i = k - 1; i >= 0; i--) {
+            dp.put(i, windowSum / maxPts);
+            double remove = 0.0;
+            if (i + maxPts <= n) {
+                remove = dp.getOrDefault(i + maxPts, 1.0);
+            }
+            windowSum += dp.get(i) - remove;
+        }
+        return dp.get(0);
+    }
+}
