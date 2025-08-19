0b960e47d6fe49708cdb2f4a21eea21973e5c64e
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-08-19 17:36:14 +0530

    LEETCODE -> 2348. Number of Zero-Filled Subarrays

---
+++ ZeroFilledSubarray.java
@@ -0,0 +1,13 @@
+class ZeroFilledSubarray {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans = 0, run = 0;
+        for (int x : nums) {
+            if (x == 0) { 
+                run++; 
+                ans += run; 
+            }
+            else run = 0;
+        }
+        return ans;
+    }
+}
